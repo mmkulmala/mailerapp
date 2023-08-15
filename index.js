9fbd157df9458668b93064e2c24ad41c5b33a6e7
Marno Kulmala <thejavasaurus@gmail.com>
2023-08-15 12:53:40 +0300

    oauth and routes

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express')
-const app = express()
-const port = process.env.PORT || 3000
+require('./services/passport')
 
-app.get('/', (req, res) => {
-    res.send({ hi: 'there' })
-})
+const port = process.env.PORT || 5000
+
+const app = express()
+
+require('./routes/authRoutes')(app)
 
 app.listen(port, () => console.log(`Mailerapp server listening on port ${port}!`))
